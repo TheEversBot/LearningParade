7a4d16f7c58dcfce9a350d4a2e62490ca90fdbe9
Luke Evers <43394258+TheEversBot@users.noreply.github.com>
2018-09-18 20:10:01 -0700

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,29 @@
+name=raw_input('Please enter your name\n')
+num1=input('Please enter a number\n')
+print '****CoMpUtInG*****'
+print '****cOmPuTiNg*****'
+print '****CoMpUtInG*****'
+print '****cOmPuTiNg*****'
+print '****CoMpUtInG*****'
+print '****cOmPuTiNg*****'
+x='Hello, '
+x1=' You picked '
+x2=' and.....'
+print x,name,x1,num1,x2
+if num1 == 0 :
+    print'Your number is zero'
+elif num1 % 4 == 0 :
+    print'Your number is divisible by 4'
+else :
+    print'Your number is not divisible by 4'
+if num1 > 0 :
+    print'Your number is greater than 0'
+if num1 < 10 :
+    print'Your number is less than ten'
+else :
+    print'Your number is greater than ten'
+if num1 %2 == 0 :
+    print'Your number is even'
+else :
+    print'Your number is odd'
+print 'COPYRIGHT FREDWARETEK. WE WILL SUE. WARNING WARNING.'
