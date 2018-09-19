3afa962ee04c6c51d7e0402cebc0320e89b63ff5
TheEversBot <43394258+TheEversBot@users.noreply.github.com>
2018-09-18 18:33:12 -0700

    Add files via upload

---
+++ ctof.py
@@ -0,0 +1,35 @@
+print '**Hello, welcome to Freds Celcius to Farenheight converter**\n'
+b=input('Please enter your temperature in celcius!\n')
+d=(b*9)/5+32
+a='your input of '
+c='    resulted in a conversion to' 
+e='degrees farenheight'
+x='degrees celcius'
+print a,b,x
+print c
+print d,e
+print '                      ________________'
+print '                      \      __      /         __'
+print '                       \_____()_____/         /  )'
+print "                       '============`        /  /"
+print "                        #---\  /---#        /  /"
+print "                       (# @\| |/@  #)      /  /"
+print "                        \   (_)   /       /  /"
+print "                        |\ '---` /|      /  /"
+print "                _______/  \_____// \____/ o_|"
+print "               /       \  /     \  /   / o_|"
+print "              / |           o|        / o_| "
+print "             /  |  _____     |       / /   \ "
+print "            /   |  |===|    o|      / /\    "
+print "           |    |   \@/      |     / /  \    "
+print '           |    |___________o|__/----)   \    \/'
+print "           |     '              || --)    \     |"
+print "           |___________________||  --)     \    /"
+print "                |           o|   ''''   |   \__/"
+print "                |            |          |"
+print "COPYRIGHT FREDWARETEK. WE WILL SUE. WARNING WARNING"
+print "to use this program you must have a valid serial #"
+print "if you do not have a serial please send $7.99 to"
+print "djdeiter@gmail.com on Paypal"
+print "thanks"
+
