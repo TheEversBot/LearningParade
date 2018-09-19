1e676075770dae0022fa484f98c359ddfd144ff9
TheEversBot <43394258+TheEversBot@users.noreply.github.com>
2018-09-18 19:26:58 -0700

    Add files via upload

---
+++ PayRate.py
@@ -0,0 +1,9 @@
+print 'Welcome to Fredwaretek Pay Rate calculator\n'
+print 'please follow the instructions now\n'
+print '***********\n'
+a=input('Hows per week\n')
+b=input('Pay Rate\n')
+c=a*b
+d='Your pay rate is: $'
+print d,c
+print 'COPYRIGHT FREDWARETEK. WE WILL SUE. WARNING WARNING.'
